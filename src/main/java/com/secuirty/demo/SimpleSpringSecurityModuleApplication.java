f6b70fe0d7338aba55ec16dea8f22551222626d5
Vinodambarapu267 <ambarapuvinod@gmail.com>
2026-03-11 21:44:59 +0530

    initial commit

---
+++ src/main/java/com/secuirty/demo/SimpleSpringSecurityModuleApplication.java
@@ -0,0 +1,13 @@
+package com.secuirty.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringSecurityModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringSecurityModuleApplication.class, args);
+	}
+
+}
